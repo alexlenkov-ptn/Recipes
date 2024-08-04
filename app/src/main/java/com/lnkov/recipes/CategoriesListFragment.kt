62a6a2f3cc9eae9972f5131080e052ea7f895e88
alexlenkov-ptn <alex.lenkov123@outlook.com>
2024-08-04 22:06:55 +0300

    Create Fragment, create ViewBinding in CategoryListFragment

---
+++ app/src/main/java/com/lnkov/recipes/CategoriesListFragment.kt
@@ -0,0 +1,29 @@
+package com.lnkov.recipes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.lnkov.recipes.databinding.FragmentListCategoriesBinding
+
+class CategoriesListFragment : Fragment() {
+    private var _binding: FragmentListCategoriesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentListCategoriesBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
