22c4bdb47332820afd107fb7ea8c2de1631815da
avlenkov <alex.lenkov@mail.ru>
2024-09-19 00:42:32 +0300

    RA-09

--- app/src/main/java/com/lnkov/recipes/RecipesListFragment.kt
+++ app/src/main/java/com/lnkov/recipes/RecipesListFragment.kt
@@ -1,14 +1,13 @@
 package com.lnkov.recipes
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.lnkov.recipes.databinding.FragmentListCategoriesBinding
+import com.lnkov.recipes.databinding.FragmentRecipesListBinding
 
 class RecipesListFragment : Fragment() {
-    private val binding by lazy { FragmentListCategoriesBinding.inflate(layoutInflater) }
+    private val binding by lazy { FragmentRecipesListBinding.inflate(layoutInflater) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
