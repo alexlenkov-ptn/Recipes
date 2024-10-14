2c91aaabb8d31f9a4c483380b4632f662358b470
avlenkov <alex.lenkov@mail.ru>
2024-10-14 08:23:19 +0300

    RA-14

--- app/src/main/java/com/lnkov/recipes/Constants.kt
+++ app/src/main/java/com/lnkov/recipes/Constants.kt
@@ -5,6 +5,4 @@ object Constants {
     const val ARG_CATEGORY_NAME = "ARG_CATEGORY_NAME"
     const val ARG_CATEGORY_IMAGE_URL = "ARG_CATEGORY_IMAGE_URL"
     const val ARG_RECIPE = "ARG_RECIPE"
-    const val PADDING_TWELVE = 12
-    const val PADDING_SIXTEEN = 16
 }
