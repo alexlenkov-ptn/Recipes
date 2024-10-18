33b45d0457700435a453fdb685cf31e580c3c578
avlenkov <alex.lenkov@mail.ru>
2024-10-19 00:17:58 +0300

    RA-17

--- app/src/main/java/com/lnkov/recipes/RecipeFragment.kt
+++ app/src/main/java/com/lnkov/recipes/RecipeFragment.kt
@@ -2,7 +2,6 @@ package com.lnkov.recipes
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.SharedPreferences
 import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
@@ -53,12 +52,8 @@ class RecipeFragment : Fragment() {
 
     private fun initUI(recipe: Recipe) {
         var heartIconStatus = false
-
-        var favoritesSet : MutableSet<String?>? = mutableSetOf(null)
-        if (getFavorites() != null) favoritesSet = getFavorites()
-
+        var favoritesSet: MutableSet<String?>? = getFavorites()
         val recipeIdString = recipe.id.toString()
-
 
         val drawable: Drawable? = try {
             Drawable.createFromStream(
@@ -76,23 +71,19 @@ class RecipeFragment : Fragment() {
             tvRecipe.text = recipe.title
 
             ibIcHeart.apply {
-                setImageResource(R.drawable.ic_heart_empty_recipe)
 
                 if (favoritesSet?.contains(recipeIdString) == true) {
                     heartIconStatus = true
                     setImageResource(R.drawable.ic_heart_recipe)
-                }
-                else {
+                } else {
                     heartIconStatus = false
+                    setImageResource(R.drawable.ic_heart_empty_recipe)
                 }
-
-// todo я вообще чего-то забыл по какому принципу у нас включается иконка
-                // todo нужно пересмотреть RA-16 и эту реализацию
 
                 setOnClickListener {
                     heartIconStatus = !heartIconStatus
 
-                    when(heartIconStatus) {
+                    when (heartIconStatus) {
                         true -> {
                             setImageResource(R.drawable.ic_heart_recipe)
                             favoritesSet?.add(recipeIdString)
@@ -151,7 +142,9 @@ class RecipeFragment : Fragment() {
     fun saveFavorites(stringSet: Set<String>) {
 
         val sharePrefs = context?.getSharedPreferences(
-            getString(R.string.com_lnkov_recipes_FAVORITES_FILE_KEY), Context.MODE_PRIVATE) ?: return
+            getString(R.string.com_lnkov_recipes_FAVORITES_FILE_KEY),
+            Context.MODE_PRIVATE
+        ) ?: return
 
         with(sharePrefs.edit()) {
             putStringSet("favorites_key", stringSet)
@@ -159,14 +152,14 @@ class RecipeFragment : Fragment() {
         }
     }
 
-    fun getFavorites() : MutableSet<String?>? {
+    fun getFavorites(): MutableSet<String?>? {
         val sharePrefs = context?.getSharedPreferences(
-            getString(R.string.com_lnkov_recipes_FAVORITES_FILE_KEY), Context.MODE_PRIVATE) ?: return null
-
-        val stringHashSet : Set<String>  = HashSet<String>()
+            getString(R.string.com_lnkov_recipes_FAVORITES_FILE_KEY),
+            Context.MODE_PRIVATE
+        ) ?: return null
+        val stringHashSet: Set<String> = HashSet<String>()
 
         return sharePrefs.getStringSet("favorites_key", stringHashSet)
-
     }
 
 
