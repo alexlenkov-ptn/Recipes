a4bac94d08382dbdfc17649f85efd8eab0cdec26
avlenkov <alex.lenkov@mail.ru>
2024-10-28 10:19:34 +0300

    RA-23 p3 finished RecipeViewModel.kt added getFavorites

--- app/src/main/java/com/lnkov/recipes/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/lnkov/recipes/ui/recipes/recipe/RecipeViewModel.kt
@@ -3,18 +3,17 @@ package com.lnkov.recipes.ui.recipes.recipe
 import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
-import android.graphics.drawable.Drawable
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.lnkov.recipes.data.Constants
 import com.lnkov.recipes.data.STUB
-import com.lnkov.recipes.model.Recipe
 import com.lnkov.recipes.model.RecipeUiState
 
 class RecipeViewModel(application: Application) : AndroidViewModel(application) {
     private val _recipeUiState = MutableLiveData<RecipeUiState>()
+    private val recipeId = 0
 
     private val sharePrefs by lazy {
         application.getSharedPreferences(
@@ -25,25 +24,7 @@ class RecipeViewModel(application: Application) : AndroidViewModel(application) 
 
     init {
         Log.i("!!!", "RecipeViewModel created")
-        _recipeUiState.value = RecipeUiState()
         updateHeartIconStatus(true)
-        val recipe = loadRecipe(0) // todo recipeId получить из bundle
-
-        val drawable: Drawable? = try {
-            Drawable.createFromStream(
-                application.assets?.open(recipe?.imageUrl ?: ""),
-                null
-            )
-        } catch (e: Exception) {
-            Log.d("!!!", "Image not found: ${recipe?.imageUrl}")
-            null
-        }
-
-        _recipeUiState.value = _recipeUiState.value?.copy(
-            recipe = recipe,
-            drawable = drawable,
-        )
-
     }
 
     val recipeUiState: LiveData<RecipeUiState>
@@ -61,8 +42,18 @@ class RecipeViewModel(application: Application) : AndroidViewModel(application) 
         _recipeUiState.value = _recipeUiState.value?.copy(numberOfPortions = newNumberOfPortions)
     }
 
-    fun loadRecipe(recipeId: Int): Recipe? {
-        return STUB.getRecipeById(0, recipeId)
+    fun loadRecipe() {
+        val recipe = STUB.getRecipeById(0, recipeId)
+        val heartIconStatus = getFavorites(sharePrefs).contains(recipeId.toString())
+
+        _recipeUiState.value = _recipeUiState.value?.copy(
+            recipe = recipe,
+            heartIconStatus = heartIconStatus
+        )
+
+        _recipeUiState.value = RecipeUiState()
+
+        // TODO: пункт 4 доделать
     }
 
     fun getFavorites(sharePrefs: SharedPreferences?): MutableSet<String> {
