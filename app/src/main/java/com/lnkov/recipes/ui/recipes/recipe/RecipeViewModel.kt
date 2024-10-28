89806e8074d951c3fcfe99d6757cc8d513d4bc91
avlenkov <alex.lenkov@mail.ru>
2024-10-28 10:01:44 +0300

    RA-23 p2 finished RecipeViewModel.kt created fun loadRecipe
    # Conflicts:
    #	app/src/main/java/com/lnkov/recipes/ui/recipes/recipe/RecipeViewModel.kt

--- app/src/main/java/com/lnkov/recipes/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/lnkov/recipes/ui/recipes/recipe/RecipeViewModel.kt
@@ -1,12 +1,20 @@
 package com.lnkov.recipes.ui.recipes.recipe
 
+import android.app.Application
+import android.content.SharedPreferences
+import android.graphics.drawable.Drawable
 import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.lnkov.recipes.model.Recipe
 import android.graphics.drawable.Drawable
 
+import com.lnkov.recipes.data.Constants
+import com.lnkov.recipes.data.STUB
+import com.lnkov.recipes.model.Recipe
+import com.lnkov.recipes.model.RecipeUiState
 
 class RecipeViewModel : ViewModel() {
     data class RecipeUiState(
@@ -21,6 +29,12 @@ class RecipeViewModel : ViewModel() {
     init {
         Log.i("!!!", "RecipeViewModel created")
         updateHeartIconStatus(true)
+        val recipe = loadRecipe(0) // todo recipeId получить из bundle
+
+        _recipeUiState.value = _recipeUiState.value?.copy(
+            recipe = recipe,
+        )
+
     }
 
     val recipeUiState: LiveData<RecipeUiState>
@@ -36,6 +50,10 @@ class RecipeViewModel : ViewModel() {
 
     private fun updateNumberOfPortions(newNumberOfPortions: Int) {
         _recipeUiState.value = _recipeUiState.value?.copy(numberOfPortions = newNumberOfPortions)
+    }
+
+    fun loadRecipe(recipeId: Int): Recipe? {
+        return STUB.getRecipeById(0, recipeId)
     }
 
 }
