69c509baa58e82b62e15fdf8fdc9ac3ac753e2df
avlenkov <alex.lenkov@mail.ru>
2024-10-28 10:01:44 +0300

    RA-23 p2 finished RecipeViewModel.kt created fun loadRecipe

--- app/src/main/java/com/lnkov/recipes/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/lnkov/recipes/ui/recipes/recipe/RecipeViewModel.kt
@@ -1,18 +1,42 @@
 package com.lnkov.recipes.ui.recipes.recipe
 
+import android.app.Application
+import android.content.SharedPreferences
+import android.graphics.drawable.Drawable
 import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import com.lnkov.recipes.data.Constants
+import com.lnkov.recipes.data.STUB
+import com.lnkov.recipes.model.Recipe
 import com.lnkov.recipes.model.RecipeUiState
 
-class RecipeViewModel : ViewModel() {
+class RecipeViewModel(application: Application) : AndroidViewModel(application) {
     private val _recipeUiState = MutableLiveData<RecipeUiState>()
+
 
     init {
         Log.i("!!!", "RecipeViewModel created")
         _recipeUiState.value = RecipeUiState()
         updateHeartIconStatus(true)
+        val recipe = loadRecipe(0) // todo recipeId получить из bundle
+
+        val drawable: Drawable? = try {
+            Drawable.createFromStream(
+                application.assets?.open(recipe?.imageUrl ?: ""),
+                null
+            )
+        } catch (e: Exception) {
+            Log.d("!!!", "Image not found: ${recipe?.imageUrl}")
+            null
+        }
+
+        _recipeUiState.value = _recipeUiState.value?.copy(
+            recipe = recipe,
+            drawable = drawable,
+        )
+
     }
 
     val recipeUiState: LiveData<RecipeUiState>
@@ -28,6 +52,10 @@ class RecipeViewModel : ViewModel() {
 
     fun updateNumberOfPortions(newNumberOfPortions: Int) {
         _recipeUiState.value = _recipeUiState.value?.copy(numberOfPortions = newNumberOfPortions)
+    }
+
+    fun loadRecipe(recipeId: Int): Recipe? {
+        return STUB.getRecipeById(0, recipeId)
     }
 
 }
