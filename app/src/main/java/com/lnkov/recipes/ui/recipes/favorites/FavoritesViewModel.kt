af1878e0d1c8a10089e1273804db71518bddc371
alexlenkov-ptn <alex.lenkov123@outlook.com>
2024-11-05 21:45:34 +0300

    RA-27 add FavoritesViewModel.kt

---
+++ app/src/main/java/com/lnkov/recipes/ui/recipes/favorites/FavoritesViewModel.kt
@@ -0,0 +1,10 @@
+package com.lnkov.recipes.ui.recipes.favorites
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class FavoritesViewModel(
+    application: Application
+) : AndroidViewModel(application) {
+
+}
