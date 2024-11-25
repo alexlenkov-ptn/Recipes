37b19c459f09d026134391077666f413bac5271f
avlenkov <alex.lenkov@mail.ru>
2024-11-25 22:02:45 +0300

    RA-38

--- app/src/main/java/com/lnkov/recipes/ui/categories/CategoriesDao.kt
+++ app/src/main/java/com/lnkov/recipes/ui/categories/CategoriesDao.kt
@@ -2,6 +2,7 @@ package com.lnkov.recipes.ui.categories
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.lnkov.recipes.model.Category
 
@@ -11,6 +12,6 @@ interface CategoriesDao {
     @Query("SELECT * FROM Category")
     fun getAll(): List<Category>
 
-    @Insert
-    fun addCategory(vararg categories: Category)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addCategory(categories: List<Category>)
 }
