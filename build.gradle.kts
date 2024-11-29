b12fdfec5b511717533e3de564fd883e16b99daa
avlenkov <alex.lenkov@mail.ru>
2024-11-29 19:08:37 +0300

    RA-45

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
+    alias(libs.plugins.dagger.hilt.android) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.navigation.safeargs.kotlin) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.dagger.hilt.android) apply false
 }
